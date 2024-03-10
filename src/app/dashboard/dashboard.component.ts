d956a369510b1f71a3e7b31932e8269edb8fc0ec
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-10 15:26:36 +0100

    Create a dashboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
