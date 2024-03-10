294531655290bd0c4d78e5536fe3df3281d52ea7
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-10 13:48:08 +0100

    Style the hero component and add event listener

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../hero';
+import { heroes } from '../mock-heroes';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
+  heroes = heroes;
   hero: Hero = {
     id: 1,
-    name: 'Windstorm',
+    name: 'Ayoub',
   };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
