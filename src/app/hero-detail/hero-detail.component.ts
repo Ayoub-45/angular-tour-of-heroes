b3b0157f3f67c9f92e572afa26f3d1781c062edd
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-10 14:24:48 +0100

    add a service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Hero } from '../../hero';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Hero } from '../../hero';
-import { heroes } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -9,12 +9,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = heroes;
-  hero: Hero = {
-    id: 1,
-    name: 'Ayoub',
-  };
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
